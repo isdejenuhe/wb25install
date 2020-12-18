bf55cc574b6199453b1176b5be5f253dc182c7f7
isdejenuhe <isaac.nunez@gmail.com>
2020-12-18 08:58:20 -0800

    bash para instalar el app server webdev 24

---
+++ WASInstall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+
+apt update
+apt upgrade
+apt-get -y dist-upgrade
+apt-get -y install libqtcore4 libqtgui4 libfreetype6 apache2 zip unzip
+
+mkdir /home/tmp
+
+cd /home/tmp
+
+wget https://package.windev.com/pack/wx24_77f/us/webdev/WBDEP24LINUX64PACKUS077f.zip
+
+unzip  WBDEP24LINUX64PACKUS077f.zip
+
+chmod 777 *64
+
+./webdev_update64
+
+echo "Enter password for wbuser TaOCT5w$Wqum"
+adduser wbuser
+usermod -a -G webdevadmin wbuser
+usermod -a -G wbuser www-data
+usermod -a -G webdevadmin www-data
+usermod -a -G root www-data
+mkdir /home/wbuser/data
+mkdir /home/wbuser/site
+mkdir /home/wbuser/webserviceREST
+mkdir /home/wbuser/webserviceSOAP
+mkdir /home/wbuser/ftp_webdev
+chgrp webdevadmin /home/wbuser/data
+chgrp webdevadmin /home/wbuser/site
+chgrp webdevadmin /home/wbuser/webserviceREST
+chgrp webdevadmin /home/wbuser/webserviceSOAP
+chgrp webdevadmin /home/wbuser/ftp_webdev
+chmod ug+rwx /home/wbuser/data
+chmod ug+rwx /home/wbuser/site
+chmod ug+rwx /home/wbuser/webserviceREST
+chmod ug+rwx /home/wbuser/webserviceSOAP
+chmod ug+rwx /home/wbuser/ftp_webdev
