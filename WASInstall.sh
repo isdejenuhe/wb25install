41fd8be1af078291aae5f0c5a64f82a4bc7d1a9d
isdejenuhe <55768383+isdejenuhe@users.noreply.github.com>
2022-05-06 15:11:01 -0700

    Update WASInstall.sh

--- WASInstall.sh
+++ WASInstall.sh
@@ -23,28 +23,6 @@ chmod 777 *64
 
 ./webdev_install64 
 
-echo "Enter password for wbuser"
-adduser WBUSER
-usermod -a -G webdevadmin WBUSER
-usermod -a -G WBUSER www-data
-usermod -a -G webdevadmin www-data
-usermod -a -G root www-data
-mkdir /home/WBUSER/data
-mkdir /home/WBUSER/site
-mkdir /home/WBUSER/webserviceREST
-mkdir /home/WBUSER/webserviceSOAP
-mkdir /home/WBUSER/ftp_webdev
-chgrp webdevadmin /home/wbuser/data
-chgrp webdevadmin /home/wbuser/site
-chgrp webdevadmin /home/wbuser/webserviceREST
-chgrp webdevadmin /home/wbuser/webserviceSOAP
-chgrp webdevadmin /home/wbuser/ftp_webdev
-chmod ug+rwx /home/WBUSER/data
-chmod ug+rwx /home/WBUSER/site
-chmod ug+rwx /home/WBUSER/webserviceREST
-chmod ug+rwx /home/WBUSER/webserviceSOAP
-chmod ug+rwx /home/WBUSER/ftp_webdev
-
 cd /home
 
 rm -rf tmp
