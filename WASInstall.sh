ccc41512876f96be7a49cae76da5dd9c26cbcf63
isdejenuhe <55768383+isdejenuhe@users.noreply.github.com>
2022-08-05 12:46:58 -0700

    Update WASInstall.sh

--- WASInstall.sh
+++ WASInstall.sh
@@ -3,7 +3,7 @@
 apt update
 apt upgrade
 apt-get -y dist-upgrade
-apt-get -y install build-essential qt5-default libqt5gui5 libfreetype6 apache2 zip unzip ttf-mscorefonts-installer libapache2-mod-fcgid zlib1g-dev freetype2* ttf-*
+apt-get -y install build-essential ca-certificates libqt5widgets5 qt5-default libqt5gui5 libfreetype6 apache2 zip unzip ttf-mscorefonts-installer libapache2-mod-fcgid zlib1g-dev freetype2* ttf-*
 apt autoremove
 apt-get autoremove
 
@@ -12,12 +12,12 @@ ln -s /usr/lib/libfreetype.so.6 /usr/lib/libfreetype.so
 mkdir /home/tmp
 
 cd /home/tmp
+     
+wget https://package.windev.com/pack/wx27/install/wx27_103s/us/WBDEP27LINUXPACKDVDUS103s.zip?s=N27A012673
 
-wget https://package.windev.com/pack/wx26/install/wx26_101d/us/WBDEP26LINUXPACKDVDUS101d.zip?s=N27A012673
+mv WBDEP27LINUXPACKDVDUS103s.zip\?s\=N27A012673  WBDEP27LINUXPACKDVDUS103s.zip
 
-mv WBDEP26LINUXPACKDVDUS101d.zip\?s\=N27A012673  WBDEP26LINUXPACKDVDUS101d.zip
-
-unzip WBDEP26LINUXPACKDVDUS101d.zip
+unzip WBDEP27LINUXPACKDVDUS103s.zip
 
 cd Linux64x86/
 
