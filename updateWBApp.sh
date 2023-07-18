57ab956a55bc7a56133e5b3f379181397cd0c02c
isdejenuhe <55768383+isdejenuhe@users.noreply.github.com>
2023-07-17 17:00:28 -0700

    Update updateWBApp.sh
    se actualizo para descargar el app server 28

--- updateWBApp.sh
+++ updateWBApp.sh
@@ -2,11 +2,11 @@ mkdir /home/tmp
 
 cd /home/tmp
      
-wget https://package.windev.com/pack/wx27/install/wx27_104h/us/WBDEP27LINUXPACKDVDUS104h.zip?s=N27A012673
+wget https://package.windev.com/pack/wx28/install/wx28_94s/us/WBDEP28LINUXPACKDVDUS094s.zip?s=N28A012834
 
-mv WBDEP27LINUXPACKDVDUS104h.zip\?s\=N27A012673  WBDEP27LINUXPACKDVDUS104h.zip
+mv WBDEP28LINUXPACKDVDUS094s.zip\?s\=N28A012834  WBDEP28LINUXPACKDVDUS094s.zip
 
-unzip WBDEP27LINUXPACKDVDUS104h.zip
+unzip WBDEP28LINUXPACKDVDUS094s.zip
 
 cd Linux64x86/
 
@@ -16,6 +16,4 @@ chmod 777 *64
 
 cd /home
 
-rm -rf tmp
-
-rm -rf wb27install
+rm -rfv tmp
