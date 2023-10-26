22ef36bbf94a64181792889bbcfa66f8e5a63805
isdejenuhe <55768383+isdejenuhe@users.noreply.github.com>
2023-10-26 09:11:18 -0700

    Update updateWBApp.sh
    se actualizo a la version 95g

--- updateWBApp.sh
+++ updateWBApp.sh
@@ -2,11 +2,11 @@ mkdir /home/tmp
 
 cd /home/tmp
      
-wget https://package.windev.com/pack/wx28/install/wx28_94s/us/WBDEP28LINUXPACKDVDUS094s.zip?s=N28A012834
+wget https://package.windev.com/pack/wx28/install/wx28_95g/us/WBDEP28LINUXPACKDVDUS095g.zip?s=N28A012834
 
-mv WBDEP28LINUXPACKDVDUS094s.zip\?s\=N28A012834  WBDEP28LINUXPACKDVDUS094s.zip
+mv WBDEP28LINUXPACKDVDUS095g.zip\?s\=N28A012834  WBDEP28LINUXPACKDVDUS095g.zip
 
-unzip WBDEP28LINUXPACKDVDUS094s.zip
+unzip WBDEP28LINUXPACKDVDUS095g.zip
 
 chmod 777 *64
 
